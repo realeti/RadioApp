f1734e9bf986952fa1974efaeeeb97dcd1481386
Maria Nesterova <82315940+ridebyhorse@users.noreply.github.com>
2024-07-28 19:05:16 +0300

    base project, add some assets

---
+++ RadioApp/RadioApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  RadioApp
+//
+//  Created by Мария Нестерова on 28.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .tealApp
+    }
+
+}
+
